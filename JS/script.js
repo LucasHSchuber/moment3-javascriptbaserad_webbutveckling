0a529f952a23a78d34b2c8bed30f36bab4711aa2
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-10-31 08:27:30 +0100

    delete works

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+
+
+//Skriv ut courses till DOM
 function getCourses() {
     fetch('http://localhost:3000/api/courses')
         .then(response => response.json())
@@ -9,19 +12,90 @@ function getCourses() {
                     <td>${course.courseId}</td>
                     <td>${course.courseName}</td>
                     <td>${course.coursePeriod}</td>
-                    <td><button class="del-btn" onClick="DeleteCourse(${course._id});">Radera</button></td>
+                    <td><button class="del-btn delete" onClick="deleteCourse(${course._id})" data-id="${course._id}">Radera</button></td>
                 </tr> `
             );
         })
         .catch(error => {
             console.error('Error fetching courses:', error);
         });
-}
-function DeleteCourse(id) {
-    console.log("deleted " + id);
 
-    
-    getCourses();
+    // const deleteButtons = document.querySelectorAll('.delete');
+
+
+    // deleteButtons.forEach(button => {
+    //     button.addEventListener('click', function() {
+    //         // Retrieve the data-id attribute value of the clicked button
+    //         const courseId = this.getAttribute('data-id');
+    //         // Perform actions based on courseId, such as deleting the corresponding course
+    //         deleteCourse(courseId);
+    //     });
+    // });
 }
+
+
+
+function deleteCourse(id) {
+
+    console.log("working " + id);
+
+    fetch(`http://localhost:3000/api/courses/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            //load alla courses id delete success
+            getCourses();
+            return response.json();
+        })
+        .then(data => {
+            console.log("Course deleted successfully:", data);
+            getCourses();
+        })
+        .catch(error => {
+            console.error("Error deleting course:", error);
+        });
+}
+
+
+// function deleteCourse(courseId) {
+
+//     fetch(`http://localhost:3000/api/courses/${courseId}`, {
+//         method: "DELETE",
+//         headers: {
+//             "content-type": "application/json",
+//         }
+//     })
+//         .then(Response => Response.json())
+//         .then(data => {
+
+//             console.log("workiiing!");
+//         })
+//         .catch(err => console.log(err))
+// }
+
+
+// async function deleteCourse(courseId) {
+//     try {
+//         const response = fetch(`http://localhost:3000/api/courses/${courseId}`, {
+//             method: 'DELETE',
+//         });
+
+//         if (response.ok) {
+//             console.log(`Course with ID ${courseId} deleted successfully.`);
+//             getCourses(); // Uppdatera kurser efter borttagning
+//         } else {
+//             console.error(`Failed to delete course with ID ${courseId}.`);
+//         }
+//     } catch (error) {
+//         console.error('Error deleting course:', error);
+//     }
+// }
 
 getCourses();
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require('express');
 const cors = require('cors'); // Import the cors package
 const app = express();
@@ -6,24 +8,38 @@ const PORT = 3000;
 // Enable CORS for all routes
 app.use(cors());
 
-
-const courses = [
-  { "_id": 1, "courseId": "DT162G", "courseName": "Javascript-baserad webbutveckling", "coursePeriod": 1 },
-  { "_id": 2, "courseId": "IK060G", "courseName": "Projektledning", "coursePeriod": 1 },
-  { "_id": 3, "courseId": "DT071G", "courseName": "Programmering i C#.NET", "coursePeriod": 2 },
-  { "_id": 4, "courseId": "DT148G", "courseName": "Webbutveckling för mobila enheter", "coursePeriod": 2 },
-  { "_id": 5, "courseId": "DT102G", "courseName": "ASP.NET med C#", "coursePeriod": 3 },
-  { "_id": 6, "courseId": "IG021G", "courseName": "Affärsplaner och kommersialisering", "coursePeriod": 3 },
-  { "_id": 7, "courseId": "DT069G", "courseName": "Multimedia för webben", "coursePeriod": 4 },
-  { "_id": 8, "courseId": "DT080G", "courseName": "Självständigt arbete", "coursePeriod": 4 }
-];
-
+const courses = 
+  [{ "_id": 1, "courseId": "DT162G", "courseName": "Javascript-baserad webbutveckling", "coursePeriod": 1 }, { "_id": 2, "courseId": "IK060G", "courseName": "Projektledning", "coursePeriod": 1 }, { "_id": 3, "courseId": "DT071G", "courseName": "Programmering i C#.NET", "coursePeriod": 2 }, { "_id": 4, "courseId": "DT148G", "courseName": "Webbutveckling för mobila enheter", "coursePeriod": 2 }, { "_id": 5, "courseId": "DT102G", "courseName": "ASP.NET med C#", "coursePeriod": 3 }, { "_id": 6, "courseId": "IG021G", "courseName": "Affärsplaner och kommersialisering", "coursePeriod": 3 }, { "_id": 7, "courseId": "DT069G", "courseName": "Multimedia för webben", "coursePeriod": 4 }, { "_id": 8, "courseId": "DT080G", "courseName": "Självständigt arbete", "coursePeriod": 4 }]
+;
 
 app.get('/api/courses', (req, res) => {
   res.json(courses);
 });
 
+
+app.delete('/api/courses/:id', (req, res) => {
+  const courseId = parseInt(req.params.id);
+
+  // Find the index of the course with the specified ID in the courses array
+  const courseIndex = courses.findIndex(course => course._id === courseId);
+
+  if (courseIndex !== -1) {
+    // If course with the specified ID is found, remove it from the courses array
+    courses.splice(courseIndex, 1);
+    res.sendStatus(200); // Respond with success status (OK)
+  } else {
+    res.status(404).json({ error: 'Course not found' }); // Respond with error status (Not Found)
+  }
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
